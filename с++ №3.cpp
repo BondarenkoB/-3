c1d71feea61957ae456fe2cbab847165cbbf227f
Bohdan <bohdanstep23@gmail.com>
2023-05-14 13:21:19 +0300

    done

--- с++ №3.cpp
+++ с++ №3.cpp
@@ -1,3 +1,24 @@
 ﻿#include <iostream>
+#include <Windows.h>
 
-int main()
+int main() 
+{
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+        int num;
+        int sum = 0;
+
+        std::cout << "Введіть числа (введіть 0 для обчислення суми):" << std::endl;
+
+        do {
+            std::cout << "Введіть число: ";
+            std::cin >> num;
+
+            sum += num;
+        } while (num != 0);
+
+        std::cout << "Сума введених чисел: " << sum << std::endl;
+
+        return 0;
+    }
+
