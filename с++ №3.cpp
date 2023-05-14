9e17ad63ecc51dc48dacde6a0b02cc4647364f39
Bohdan <bohdanstep23@gmail.com>
2023-05-14 10:00:57 +0300

    done

--- с++ №3.cpp
+++ с++ №3.cpp
@@ -1,3 +1,18 @@
 ﻿#include <iostream>
+#include <windows.h>    
 
-int main()
+int main() 
+{
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+    int n;
+    std::cout << "Введіть число: ";
+    std::cin >> n;
+
+    std::cout << "Числа від 0 до " << n << ":" << std::endl;
+    for (int i = 0; i <= n; i++) {
+        std::cout << i << " ";
+    }
+
+    return 0;
+}
