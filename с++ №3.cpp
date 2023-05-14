3f90a06fd7812fb157190306d2a1e49c3c85fb80
Bohdan <bohdanstep23@gmail.com>
2023-05-14 13:22:09 +0300

    done

--- с++ №3.cpp
+++ с++ №3.cpp
@@ -1,3 +1,24 @@
 ﻿#include <iostream>
+#include <Windows.h>
+int main() 
+{
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+    int a, b;
+    std::cout << "Введіть першу межу діапазону: ";
+    std::cin >> a;
+    std::cout << "Введіть другу межу діапазону: ";
+    std::cin >> b;
 
-int main()
+    int start = (a < b) ? a : b;
+    int end = (a < b) ? b : a;
+
+    int sum = 0;
+    for (int i = start; i <= end; i++) {
+        sum += i;
+    }
+
+    std::cout << "Сума чисел з діапазону " << start << " - " << end << " дорівнює " << sum << std::endl;
+
+    return 0;
+}
